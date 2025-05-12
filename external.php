28035a88b4a2dcfb5f10ff98fbaea8661d76ecad
Ayan Paul <ayancse2001@gmail.com>
2025-05-12 14:09:04 +0600

    external php updated, info showing in box

--- external.php
+++ external.php
@@ -6,7 +6,7 @@
     <title>Profile Information</title>
 </head>
 <body>
-    
+
 </body>
 </html>
 <?php
@@ -17,35 +17,37 @@ session_start();
 // echo "<br>".$_GET['uname'];
 
 //var_dump($_GET);
+echo "<div style='background-color:rgb(27, 150, 122); border: 1px solid; padding: 20px; border-radius: 10px; max-width: 600px; margin: 20px auto;'>";
+echo "<h2 style='text-align: center; color:black;'>User Information Receipt</h2>";
+echo "<ul style='list-style-type: none; padding: 0; text-align: center;'>";
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit'])){
+    
+if (!empty($_POST['fname'])) {
+    echo "<li><strong>Full Name:</strong> " . htmlspecialchars($_POST['fname']) . "</li>";
+}
+if (!empty($_POST['username'])) {
+    echo "<li><strong>Username:</strong> " . htmlspecialchars($_POST['username']) . "</li>";
+}
+if (!empty($_POST['email'])) {
+    echo "<li><strong>Email:</strong> " . htmlspecialchars($_POST['email']) . "</li>";
+}
+if (!empty($_POST['dob'])) {
+    echo "<li><strong>Date of Birth:</strong> " . htmlspecialchars($_POST['dob']) . "</li>";
+}
+if (!empty($_POST['ucity'])) {
+    echo "<li><strong>City:</strong> " . htmlspecialchars($_POST['ucity']) . "</li>";
+}
+if (!empty($_POST['color'])) {
+    echo "<li><strong>Favorite Color:</strong> " . htmlspecialchars($_POST['color']) . "</li>";
+}
+if (!empty($_POST['gender'])) {
+    echo "<li><strong>Gender:</strong> " . htmlspecialchars($_POST['gender']) . "</li>";
+}
+echo "</ul>";
 
-if ($_POST['fname'] != "") {
-echo $_POST['fname'];
+}
 
-if ($_POST['username'] != "") {
-    echo "<br>".$_POST['username'];
-}
-}
-if($_POST['email'] !=""){
-echo "<br>".$_POST['email'];
-}
-if($_POST['password'] !=""){
-echo "<br>".$_POST['password'];
-}
-if($_POST['dob'] !=""){
-echo "<br>".$_POST['dob'];
-}
-if($_POST['ucity'] !=""){
-echo "<br>".$_POST['ucity'];
-}
-if($_POST['color'] !=""){
-echo "<br>".$_POST['color'];
-}
-if($_POST['gender'] !=""){
-echo "<br>".$_POST['gender'];
-}
-}
 else
     print_r("NO DATA");
 
